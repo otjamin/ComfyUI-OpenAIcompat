07a4854128515f7a84b83e09ae70f7565aa720b8
Benjamin Otto <git@otto.bayern>
2026-03-11 14:45:45 +0100

    project scaffolding

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for openaicompat."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Benjamin Otto"""
+__email__ = "git@otto.bayern"
+__version__ = "0.0.1"
+
+from .src.openaicompat.nodes import NODE_CLASS_MAPPINGS
+from .src.openaicompat.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
